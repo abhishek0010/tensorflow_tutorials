03d22c289b67d8a47d640ef434fc7608e22811c8
Abhishek Kumar <abhishekism0010@gmail.com>
2020-02-03 02:52:32 +0530

    Update 03_polynomial_regression.py
    Used the tf.keras model building method, and modified the source code accordingly

--- python/03_polynomial_regression.py
+++ python/03_polynomial_regression.py
@@ -1,6 +1,9 @@
-"""Simple tutorial for using TensorFlow to compute polynomial regression.
+"""
+Simple tutorial for using TensorFlow to compute polynomial regression.
 
-Parag K. Mital, Jan. 2016"""
+Created by Parag K. Mital, Jan. 2016
+Modified by Abhishek Kumar, Jan 2020
+"""
 # %% Imports
 import numpy as np
 import tensorflow as tf
@@ -11,68 +14,65 @@ import matplotlib.pyplot as plt
 plt.ion()
 n_observations = 100
 fig, ax = plt.subplots(1, 1)
-xs = np.linspace(-3, 3, n_observations)
-ys = np.sin(xs) + np.random.uniform(-0.5, 0.5, n_observations)
-ax.scatter(xs, ys)
+X = np.linspace(-3, 3, n_observations)
+Y = np.sin(X) + np.random.uniform(-0.5, 0.5, n_observations)
+ax.scatter(X, Y)
 fig.show()
 plt.draw()
 
-# %% tf.placeholders for the input and output of the network. Placeholders are
-# variables which we need to fill in when we are ready to compute the graph.
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+# %% Here the values are scaled for shorter range
+X_scaled = X/max(X)
+Y_scaled = Y/max(Y)
 
-# %% Instead of a single factor and a bias, we'll create a polynomial function
-# of different polynomial degrees.  We will then learn the influence that each
-# degree of the input (X^0, X^1, X^2, ...) has on the final output (Y).
-Y_pred = tf.Variable(tf.random_normal([1]), name='bias')
-for pow_i in range(1, 5):
-    W = tf.Variable(tf.random_normal([1]), name='weight_%d' % pow_i)
-    Y_pred = tf.add(tf.multiply(tf.pow(X, pow_i), W), Y_pred)
+# %% Here the class PolymonialFeatures is used for generating required 
+# n degree polymonial for the polymonial equation.
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(degree=2) 
 
-# %% Loss function will measure the distance between our observations
-# and predictions and average over them.
-cost = tf.reduce_sum(tf.pow(Y_pred - Y, 2)) / (n_observations - 1)
+# %% The degree of coefficients which fits the curve is determined by
+# hit and trial method, or by observing the behaviour of dataset
+X_2 = poly.fit_transform(X_scaled.reshape(-1,1))
+print(X_2.shape)
+print(X_2[0])
 
-# %% if we wanted to add regularization, we could add other terms to the cost,
-# e.g. ridge regression has a parameter controlling the amount of shrinkage
-# over the norm of activations. the larger the shrinkage, the more robust
-# to collinearity.
-# cost = tf.add(cost, tf.mul(1e-6, tf.global_norm([W])))
+poly = PolynomialFeatures(degree=3) 
+                                        
+X_3 = poly.fit_transform(X_scaled.reshape(-1,1))
+print(X_3.shape)
+print(X_3[0])
 
-# %% Use gradient descent to optimize W,b
-# Performs a single step in the negative gradient
-learning_rate = 0.01
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+poly = PolynomialFeatures(degree=4)
 
-# %% We create a session to use the graph
-n_epochs = 1000
-with tf.Session() as sess:
-    # Here we tell tensorflow that we want to initialize all
-    # the variables in the graph so we can use them
-    sess.run(tf.global_variables_initializer())
+X_4 = poly.fit_transform(X_scaled.reshape(-1,1))
+print(X_4.shape)
+print(X_4[0])
 
-    # Fit all training data
-    prev_training_cost = 0.0
-    for epoch_i in range(n_epochs):
-        for (x, y) in zip(xs, ys):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
+# %% A single node model is trained using the scaled polynomial dataset
+tf.keras.backend.clear_session()
+model = tf.keras.models.Sequential(
+    tf.keras.layers.Dense(1, input_shape=[5])
+)
 
-        training_cost = sess.run(
-            cost, feed_dict={X: xs, Y: ys})
-        print(training_cost)
+model.compile(loss="mean_squared_error",
+              optimizer=tf.keras.optimizers.Adam(0.1))
 
-        if epoch_i % 100 == 0:
-            ax.plot(xs, Y_pred.eval(
-                feed_dict={X: xs}, session=sess),
-                    'k', alpha=epoch_i / n_epochs)
-            fig.show()
-            plt.draw()
+history = model.fit(X_4, Y_scaled, epochs=1000)
 
-        # Allow the training to quit if we've reached a minimum
-        if np.abs(prev_training_cost - training_cost) < 0.000001:
-            break
-        prev_training_cost = training_cost
-ax.set_ylim([-3, 3])
-fig.show()
-plt.waitforbuttonpress()
+plt.plot(history.history['loss'])
+plt.xlabel('Epochs')
+plt.ylabel('MSE Loss')
+plt.show()
+
+mse = history.history['loss'][-1]
+y_hat = model.predict(X_4)
+
+plt.figure(figsize=(12,7))
+plt.title('TensorFlow Model')
+plt.scatter(X_2[:, 1], Y_scaled, label='Data $(X, y)$')
+plt.plot(X_2[:, 1], y_hat, color='red', label='Predicted Line $y = f(X)$',linewidth=4.0)
+plt.xlabel('$X$', fontsize=20)
+plt.ylabel('$y$', fontsize=20)
+plt.text(0,0.70,'MSE = {:.3f}'.format(mse), fontsize=20)
+plt.grid(True)
+plt.legend(fontsize=20)
+plt.show()
